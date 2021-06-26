169a8e63aedbcaaa40a115d13de04c51e9ac8214
Rhys Holloway <rhyswilliamholloway@gmail.com>
2021-06-26 14:19:55 -0700

    Moved pokedex folder to top of repository Added "save" option to dex-builder

--- pokedex/builder/examples/no_audio.rs
+++ pokedex/builder/examples/no_audio.rs
@@ -3,6 +3,6 @@ use std::time::Instant;
 fn main() {
     println!("Building dex...");
     let start = Instant::now();
-    dex_builder::compile("assets/pokedex/pokemon", "assets/pokedex/moves", "assets/pokedex/items", "assets/pokedex/trainers", "output/dex.bin", false);
+    dex_builder::compile("assets/pokedex/pokemon", "assets/pokedex/moves", "assets/pokedex/items", "assets/pokedex/trainers", "output/dex.bin", false, true);
     println!("Finished in {}ms!", start.elapsed().as_millis());
 }
--- pokedex/builder/src/lib.rs
+++ pokedex/builder/src/lib.rs
@@ -13,7 +13,7 @@ pub mod trainers;
 // #[cfg(feature = "gen")]
 pub mod gen;
 
-pub fn compile<P: AsRef<std::path::Path>>(pokemon_dir: P, move_dir: P, item_dir: P, trainer_dir: P, output_file: P, include_audio: bool) -> SerializedDex {
+pub fn compile<P: AsRef<std::path::Path>>(pokemon_dir: P, move_dir: P, item_dir: P, trainer_dir: P, output_file: P, include_audio: bool, save: bool) -> SerializedDex {
     let output_file = output_file.as_ref();
 
     // #[cfg(feature = "gen")]
@@ -35,15 +35,17 @@ pub fn compile<P: AsRef<std::path::Path>>(pokemon_dir: P, move_dir: P, item_dir:
         trainers,
     };
 
-    println!("Saving to file...");
-    let size = File::create(output_file)
-        .unwrap_or_else(|err| panic!("Could not create output file at {:?} with error {}", output_file, err))
-            .write(
-            &firecore_dependencies::ser::serialize(
-                    &dex
-                ).unwrap_or_else(|err| panic!("Could not serialize data with error {}", err))
-    ).unwrap_or_else(|err| panic!("Could not write to output file with error {}", err));
-    println!("Saved {} bytes to output file!", size);
+    if save {
+        println!("Saving to file...");
+        let size = File::create(output_file)
+            .unwrap_or_else(|err| panic!("Could not create output file at {:?} with error {}", output_file, err))
+                .write(
+                &firecore_dependencies::ser::serialize(
+                        &dex
+                    ).unwrap_or_else(|err| panic!("Could not serialize data with error {}", err))
+        ).unwrap_or_else(|err| panic!("Could not write to output file with error {}", err));
+        println!("Saved {} bytes to output file!", size);
+    }
 
     dex
 }
