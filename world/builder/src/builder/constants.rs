ca39bc81fd139b3d70a01daf4ec7daf93a0f3a64
Rhys Holloway <rhyswilliamholloway@gmail.com>
2021-12-24 19:50:05 -0800

    move firecore-world to its own repository

--- world/builder/src/builder/constants.rs
+++
@@ -1,7 +0,0 @@
-use std::path::Path;
-
-use worldlib::map::manager::constants::WorldMapManagerConstants;
-
-pub fn get_constants(root_path: &Path) -> WorldMapManagerConstants {
-    ron::from_str(&std::fs::read_to_string(root_path.join("center.ron")).unwrap()).unwrap()
-}
